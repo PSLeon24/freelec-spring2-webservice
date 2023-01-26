63f385b781176932912add34686aa63ecd0ebe57
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2023-01-26 10:53:51 +0900

    Initial commit

---
+++ src/main/java/com/jojoidu/book/freelecspring2webservice/FirstApplication.java
@@ -0,0 +1,13 @@
+package com.jojoidu.book.freelecspring2webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jojoidu/book/freelecspring2webservice/FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoidu.book.freelecspring2webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
